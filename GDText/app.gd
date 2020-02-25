cacda94c1ac6a3523baad76b56fca0cda2027f90
Valerioooh <diogocsvalerio@protonmail.com>
2020-02-25 19:30:00 +0000

    Update

--- GDText/app.gd
+++ GDText/app.gd
@@ -6,22 +6,27 @@ var current_file = "Untitled"
 
 func _ready() -> void:
 	update_window_title()
+
+#File Menu
 	$FileMenu.get_popup().add_item("New File")
 	$FileMenu.get_popup().add_item("Open File")
 	$FileMenu.get_popup().add_item("Save")
 	$FileMenu.get_popup().add_item("Save as ...")
 	$FileMenu.get_popup().add_item("Quit")
-
-	$HelpMenu.get_popup().add_item("About")
-
-
 	$FileMenu.get_popup().connect("id_pressed", self, "_on_Item_pressed")
+#---------------------------------------------------------------------------------
+
+	#help menu
+	$HelpMenu.get_popup().add_item("About")
 	$HelpMenu.get_popup().connect("id_pressed", self, "_on_Item_help_pressed")
-	
-func update_window_title():
+#---------------------------------------------------------------------------------
+
+
+func update_window_title():#updates the window title adding the file name after
 	OS.set_window_title(app_name + ' - ' + current_file)
 	
-func _on_Item_pressed(id):
+
+func _on_Item_pressed(id):#Checks the options
 	var file_item_name = $FileMenu.get_popup().get_item_text(id)
 	
 	print(file_item_name + ' pressed')
@@ -37,12 +42,12 @@ func _on_Item_pressed(id):
 		new_file()
 	
 	
-func new_file():
+func new_file():#sets the file to blank and untitled
 	current_file = 'Untitled'
 	update_window_title()
 	$TextEdit.text = ''
 	
-func save_file():
+func save_file():#saves the file
 	if current_file == 'Untitled':
 		$SaveDialog.popup()
 	else:
@@ -56,8 +61,8 @@ func _on_Item_help_pressed(id):
 	if help_item_name == 'About':
 		$AboutDialog.popup()
 	
-func _on_OpenDialog_file_selected(path: String) -> void:
-	print(path)
+func _on_OpenDialog_file_selected(path: String) -> void:#opens the file to read
+	print(path)#debug
 	var f = File.new()
 	f.open(path, 1)	
 	$TextEdit.text = f.get_as_text()
@@ -65,7 +70,7 @@ func _on_OpenDialog_file_selected(path: String) -> void:
 	current_file = path
 	update_window_title()
 
-func _on_SaveDialog_file_selected(path: String) -> void:
+func _on_SaveDialog_file_selected(path: String) -> void:#saves the file 
 	var f = File.new()
 	f.open(path, 2)
 	f.store_string($TextEdit.text)
